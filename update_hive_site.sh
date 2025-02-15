36f2823ba1d29457571acbd06bc7a3072b8744e1
Methode TWIZEYIMANA <53626837+tmmethode@users.noreply.github.com>
2025-02-16 00:05:29 +0200

    Update update_hive_site.sh

--- update_hive_site.sh
+++ update_hive_site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the URL to download the hive-site.xml
-HIVE_SITE_URL="https://drive.google.com/file/d/1TQSl4QLLKMZxqLxB0PWny45UC8fXc1Oe/view?usp=drivesdk"
+HIVE_SITE_URL="https://raw.githubusercontent.com/tmmethode/coding-challenge/main/hive-site.xml"
 
 # Change to the Hive configuration directory
 cd $HIVE_HOME/conf || { echo "Directory not found"; exit 1; }
